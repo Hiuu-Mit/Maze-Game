5b3a51e2addd955622774c630e0a3f0e0ed840a2
Doan Hai Phong <hiu310808@gmail.com>
2026-01-14 00:48:29 +0700

    Add maze generator

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
 from maze import create_maze
+import pygame
 
 def main():
-    ...
+    row = 9
+    col = 9
+    maze = create_maze(row, col)
+
+    for row in maze:
+        print(row)
+        
+
+   
 if __name__ == "__main__":
     main()
--- src/maze.py
+++ src/maze.py
@@ -1,2 +1,33 @@
-def create_maze():
-    ...
+import random
+
+def create_maze(rows, cols):
+    # 1 walls
+    # 0 path
+    maze = [[1 for i in range(cols)] for i in range(rows)]
+
+    start_row = 1
+    start_col = 1
+    maze[start_row][start_col] = 3
+
+    def dfs(r, c):
+        directions = [(-2, 0), (2, 0), (0, -2), (0, 2)]
+        random.shuffle(directions)
+
+        for dr, dc in directions:
+            nr = r + dr
+            nc = c + dc
+
+            if 0 < nr < rows and 0 < nc < cols:
+                if maze[nr][nc] == 1:
+                    maze[r + dr//2][c + dc//2] = 0
+                    maze[nr][nc] = 0
+                    dfs(nr, nc)
+
+    dfs(start_row, start_col)
+    return maze
+
+
+
+    
+
+    create_maze()
